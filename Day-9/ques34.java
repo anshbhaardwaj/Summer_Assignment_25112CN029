93704f783eae167add19f98995d964712d2b14f1
anshbhaardwaj <anshbhaardwaj06@gmail.com>
2026-06-10 18:13:34 +0530

    completed my summer assignment

--- Day-9/ques34.java
+++ Day-9/ques34.java
@@ -1,10 +1,10 @@
-public class ques33 {
+public class ques34 {
     public static void main(String[] args) {
         int rows = 5;
 
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println();
         }
