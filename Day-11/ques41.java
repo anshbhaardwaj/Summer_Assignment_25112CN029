1dc9635bc103be18d1051e592860b9c4d13d2501
anshbhaardwaj <anshbhaardwaj06@gmail.com>
2026-06-11 20:41:59 +0530

    completed my summer assignment

---
+++ Day-11/ques41.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ques41{
+
+    // Function to find sum of two numbers
+    static int findSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = findSum(num1, num2);
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
