bb58791c0f6f04f556e490f860cdf775c0717715
anshbhaardwaj <anshbhaardwaj06@gmail.com>
2026-06-24 16:27:02 +0530

    completed my summmer assignment

---
+++ Day-24/ques94.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ques94 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        StringBuilder compressed = new StringBuilder();
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                compressed.append(str.charAt(i));
+                compressed.append(count);
+                count = 1;
+            }
+        }
+
+        System.out.println("Compressed String: " + compressed);
+
+        sc.close();
+    }
+}
